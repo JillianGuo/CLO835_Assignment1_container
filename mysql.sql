1eb2161e959a94cf1caff64d04a92bc911f32cc5
JillianGuo <jiyun.guo@gmail.com>
2025-01-27 00:52:46 +0000

    Added terraform code to deploy an EC2 instance

--- mysql.sql
+++ mysql.sql
@@ -8,7 +8,18 @@ last_name VARCHAR(20),
 primary_skill VARCHAR(20),
 location VARCHAR(20));
 
-INSERT INTO employee VALUES ('1','Amanda','Williams','Smile','local');
-INSERT INTO employee VALUES ('1','Alan','Williams','Empathy','alien');
-SELECT * FROM employee;
+-- Insert into employee table if the record does not already exist
+INSERT INTO employee (emp_id, first_name, last_name, primary_skill, location)
+SELECT '1', 'Amanda', 'Williams', 'Smile', 'local'
+WHERE NOT EXISTS (
+    SELECT 1 FROM employee WHERE emp_id = '1'
+);
 
+INSERT INTO employee (emp_id, first_name, last_name, primary_skill, location)
+SELECT '2', 'Alan', 'Williams', 'Empathy', 'alien'
+WHERE NOT EXISTS (
+    SELECT 1 FROM employee WHERE emp_id = '2'
+);
+
+-- Display the table contents
+SELECT * FROM employee;
